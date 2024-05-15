57463019cfb2904eeb2994aa3993ee92e2a6e0ab
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-05-15 14:00:51 -0400

    set up basic react components and created  the nav bar

---
+++ Timothy-Lee-Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header/header.jsx'
+import Nav from './components/Nav/nav.jsx'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+    
+  )
+
+}
+
+export default App
