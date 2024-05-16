e994a04e4ace282cfec10a33d73664eb6a227f25
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-05-16 12:49:11 -0400

    added a picture and paragraph to about me page

--- Timothy-Lee-Portfolio/src/components/Pages/About/about.jsx
+++ Timothy-Lee-Portfolio/src/components/Pages/About/about.jsx
@@ -1,15 +1,26 @@
+import me from '../../../assets/images/me.png'
+import './about.css'
+
 export default function About() {
     return(
         <div className="row d-flex text-center">
-            <div className="col-sm-6 border border-dark">
+            <div className="col-sm-4">
                 <h1>
-                    col 1
+                    <img src={me} className='custom-img mt-2'></img>
                 </h1>
             </div>
-            <div className="col-sm-6 border border-dark">
-                <h1>
-                    col 2
-                </h1>
+            <div className="col-sm-7 m-1">
+                <h5 className='text-left'>
+                    My name is Timothy Lee, I am a 30 year old aspiring software developer from New Hampshire.
+                    I was first introduced to software development by my father through first robotics. 
+                    My father is a developer and was the coach of the team, I had a lot of fun working on the robots and competeing.
+                    it wasn't until I turned 24 that I started really looking into a career change into tech.
+                    My first step was aquiring a computer science degree from Great Bay Community College.
+                    My next step to further my learning was taking and completeing the full stack development
+                    bootcamp through UNH. I am planning to become AWS certified once the course is completed
+                    and than I will be applying for junior dev jobs.
+
+                </h5>
             </div>
         </div>
     )
--- Timothy-Lee-Portfolio/src/components/Pages/Resume/resume.jsx
+++ Timothy-Lee-Portfolio/src/components/Pages/Resume/resume.jsx
@@ -5,21 +5,35 @@ export default function Resume(){
         <div className="">
             <div>
                 <h3>
+                    Stacks
+                </h3>
+                <ul>
+                    <li>
+                        MERN
+                    </li>
+                </ul>
+                <h3>
                     Front End Skills
                 </h3>
                 <ul>
                     <li>Javascript</li>
+                    <li>Java</li>
+                    <li>Python</li>
                     <li>Node.js</li>
                     <li>React.js</li>
                     <li>Bootstrap</li>
                     <li>Handlebars</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
                 </ul>
                 <h3>
-                    Backend Skils
+                    Backend Skills
                 </h3>
                 <ul>
                     <li>SQL</li>
                     <li>MongoDb</li>
+                    <li>MySQL</li>
+                    <li>Insomnia</li>
                     <li>Express.js</li>
                     <li>Object Relational Mapping</li>
                     <li>PWA's</li>
